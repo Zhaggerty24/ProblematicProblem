ddc607061a5b379856acd083d04b1bea4b4310fc
Zachary Haggerty <zhaggerty24@gmail.com>
2023-01-03 15:32:25 -0500

    initial commit

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -86,7 +86,7 @@ namespace ProblematicProblem
                     Console.WriteLine($"Oh no! Looks like you are too young to do {randomActivity}");
                     Console.WriteLine("Pick something else!");
                     activities.Remove(randomActivity);
-                    rng.Next(activities.Count);
+                    randomNumber = rng.Next(activities.Count);
                     randomActivity = activities[randomNumber];
                 }
                 Console.Write($"Ah got it! {userName}, your random activity is: {randomActivity}! Is this ok or do you want to grab another activity? Keep/Redo: ");
